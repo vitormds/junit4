cd644f639b7b2b18cb9616d554e207b5da390767
Vitor Souza <dev_vitorsouza@hotmail.com>
2023-11-20 23:16:45 -0300

    organizacao dos arquivos de teste

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -14,6 +14,7 @@ import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoService {
 	
+	
 	public Locacao alugarFilme(Usuario usuario, Filme filme) {
 		Locacao locacao = new Locacao();
 		locacao.setFilme(filme);
@@ -32,20 +33,5 @@ public class LocacaoService {
 		return locacao;
 	}
 	
-	@Test
-	public void teste() {
-		//cenario
-		 LocacaoService service = new LocacaoService();
-		 Usuario usuario = new Usuario("Usuario 1");
-		 Filme filme = new Filme("Filme 1", 2, 5.0);
-		 
-		 //acao
-		 Locacao locacao = service.alugarFilme(usuario, filme);
-		 
-		 //verificacao
-		 Assert.assertTrue(locacao.getValor() == 5.0);
-		 Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		 Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), DataUtils.obterDataComDiferencaDias(0)));
-		 
-	}
+
 }
