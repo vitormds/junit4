e3b4773bc2c3166e3a6896cab6e6da4ce8529544
Vitor Souza <dev_vitorsouza@hotmail.com>
2023-11-22 21:36:38 -0300

    Formas de dividir um teste

--- src/main/java/br/ce/wcaquino/entidades/Locacao.java
+++ src/main/java/br/ce/wcaquino/entidades/Locacao.java
@@ -40,4 +40,53 @@ public class Locacao {
 	public void setFilme(Filme filme) {
 		this.filme = filme;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dataLocacao == null) ? 0 : dataLocacao.hashCode());
+		result = prime * result + ((dataRetorno == null) ? 0 : dataRetorno.hashCode());
+		result = prime * result + ((filme == null) ? 0 : filme.hashCode());
+		result = prime * result + ((usuario == null) ? 0 : usuario.hashCode());
+		result = prime * result + ((valor == null) ? 0 : valor.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Locacao other = (Locacao) obj;
+		if (dataLocacao == null) {
+			if (other.dataLocacao != null)
+				return false;
+		} else if (!dataLocacao.equals(other.dataLocacao))
+			return false;
+		if (dataRetorno == null) {
+			if (other.dataRetorno != null)
+				return false;
+		} else if (!dataRetorno.equals(other.dataRetorno))
+			return false;
+		if (filme == null) {
+			if (other.filme != null)
+				return false;
+		} else if (!filme.equals(other.filme))
+			return false;
+		if (usuario == null) {
+			if (other.usuario != null)
+				return false;
+		} else if (!usuario.equals(other.usuario))
+			return false;
+		if (valor == null) {
+			if (other.valor != null)
+				return false;
+		} else if (!valor.equals(other.valor))
+			return false;
+		return true;
+	}
+	
+	
 }
